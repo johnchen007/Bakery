1b77763fc25f8048d41aab00d5802b0d5cec28dd
johnchen007 <153539429+johnchen007@users.noreply.github.com>
2023-12-20 21:18:34 -0500

    Console Chat Bot

---
+++ src/Lease.java
@@ -0,0 +1,38 @@
+
+public class Lease {
+	double interestRate;
+	double downPayment;
+	int terms;
+	
+   public double calculateMonthlyPayment(int principal ,double interestRate,double downPayment,int terms) {
+
+	   double monthlyInterestRate = interestRate / 12 / 100;
+	    int numberOfPayments = terms * 12;
+
+	    double monthlyPayment = (principal - downPayment) * monthlyInterestRate /
+	            (1 - Math.pow(1 + monthlyInterestRate, -numberOfPayments));
+
+	    return monthlyPayment;
+    }
+
+	
+	public double getInteresRate() {
+		return interestRate;
+	}
+	public void setInteresRate(double interesRate) {
+		this.interestRate = interesRate;
+	}
+
+	public double getDownPayment() {
+		return downPayment;
+	}
+	public void setDownPayment(double downPayment) {
+		this.downPayment = downPayment;
+	}
+	public int getTerms() {
+		return terms;
+	}
+	public void setTerms(int terms) {
+		this.terms = terms;
+	}
+}
